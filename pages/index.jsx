27f0918af826ff6ce4031da2340acc7aa172189c
Ahmed Frag <a.frag166@gmail.com>
2025-03-09 20:29:09 +0200

    Adding First Pages

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+// our-domain.com/
+
+const HomePage = () => {
+  return (
+    <h1>
+        the Home Page
+    </h1>
+  );
+}
+
+export default HomePage;
---
+++ pages/news.jsx
@@ -0,0 +1,11 @@
+// our-domain.com/news
+
+const NewsPage = () => {
+  return (
+    <h1>
+        the News Page
+    </h1>
+  );
+}
+
+export default NewsPage;
